0c942e6743cdfc3654f2ce09874661d0b6f6019d
Mir Adnan <miradnanali@gmail.com>
2017-03-29 05:54:58 +0530

    Create HttpClientInterface.php

---
+++ src/OAuth2/HttpClientInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Twin23\OAuth2;
+
+/**
+ * Description of HttpClientInterface
+ *
+ * @author miradnan
+ */
+class HttpClientInterface extends \GuzzleHttp\ClientInterface {
+    
+}
