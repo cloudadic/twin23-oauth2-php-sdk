9b70f1b3113b5007909b5db968c2d1fe212ea1cf
Mir Adnan <miradnanali@gmail.com>
2017-03-29 05:53:12 +0530

    Create IdentityProviderException.php

---
+++ src/Exception/IdentityProviderException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Twin23\Exception;
+
+/**
+ * Description of IdentityProviderException
+ *
+ * @author miradnan
+ */
+class IdentityProviderException extends \Exception {
+    //put your code here
+}
