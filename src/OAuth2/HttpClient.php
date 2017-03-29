77403ff13e11d839d661ef7cdf914bcbdbf6c497
Mir Adnan <miradnanali@gmail.com>
2017-03-29 05:54:45 +0530

    Create HttpClient.php

---
+++ src/OAuth2/HttpClient.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Twin23\OAuth2;
+
+use GuzzleHttp\Exception\BadResponseException;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Description of HttpClient
+ *
+ * @author miradnan
+ */
+class HttpClient extends \GuzzleHttp\Client {
+    
+}
